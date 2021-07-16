e99dd79620d2a1bc6483f05658f490fb1aafbbc9
Louis Lecoq <llecoq@z3r2p1.42lyon.fr>
2021-07-16 11:02:38 +0200

    ft_echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: llecoq <llecoq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 18:07:59 by llecoq            #+#    #+#             */
-/*   Updated: 2021/07/12 16:32:35 by llecoq           ###   ########.fr       */
+/*   Updated: 2021/07/16 11:01:39 by llecoq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,27 @@
 // "-e not supported, usage = echo -p <msg>"
 // ou simplement ne rien faire ?
 // echo * fais un ls trimmé 
-// int	ft_echo(t_shell *shell, char **argv)
-// {
-// 	char	*full_argument;
-// 	int		flag;
+int	ft_echo(t_shell *shell, char **argv)
+{
+	char	*full_argument;
+	int		flag;
 
-// 	(void)shell;
-// 	flag = 0;
-// 	full_argument = NULL;
-// 	if (argv[1] && ft_strncmp(argv[1], "-p", 3) == 0)
-// 	{
-// 		full_argument = join_args(argv, "-p");
-// 		flag = 1;
-// 	}
-// 	else if (argv[1])
-// 		full_argument = join_args(argv, NULL);
-// 	if (full_argument)
-// 	{
-// 		ft_printf(1, "%s", full_argument);
-// 		free_set_null((void **)&full_argument);
-// 	}
-// 	if (flag == 0);
-// 		ft_putchar_fd('\n', 1);
-// 	return (1);
-// }
+	(void)shell;
+	flag = 0;
+	full_argument = NULL;
+	if (argv[1] && ft_strncmp(argv[1], "-p", 3) == 0)
+	{
+		full_argument = join_args(argv, "-p");
+		flag = 1;
+	}
+	else if (argv[1])
+		full_argument = join_args(argv, NULL);
+	if (full_argument)
+	{
+		ft_printf(1, "%s", full_argument);
+		free_set_null((void **)&full_argument);
+	}
+	if (flag == 0)
+		ft_putchar_fd('\n', 1);
+	return (1);
+}
